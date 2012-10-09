d7252e013a3ff645699b4fdc5026c2cdeea38a27
gnysek <gnysek@gmail.com>
2012-10-09 16:43:40 +0200

    view for projects

--- protected/views/projects/_view.php
+++ protected/views/projects/_view.php
@@ -1,27 +1,25 @@
 <?php
 /* @var $this ProjectsController */
 /* @var $data Projects */
-$md = new CMarkdown();
+//$md = new CMarkdown();
 ?>
 
-<div class="view">
+<div class="row">
+	<div class="seven columns" style="padding-left: 10px; border-left: 20px solid <?= (!empty($data->color)) ? $data->color : 'black' ?>;">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
-	<br />
+		<div class="right">
+			<?php echo CHtml::link('Update', array('update', 'id' => $data->id), array('class' => 'button secondary tiny')); ?>
+			<?php echo CHtml::link('Delete', array('delete', 'id' => $data->id), array('class' => 'button secondary tiny')); ?>
+		</div>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
-	<?php echo CHtml::encode($data->name); ?>
-	<br />
+		<p>#<?= $data->id ?>. <strong><?php echo CHtml::link(CHtml::encode($data->name), array('view', 'id' => $data->id)); ?></strong></p>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('wiki')); ?>:</b>
-	<?php echo $md->transform($data->wiki); ?>
-	<br />
-	
-	<b><?php echo CHtml::encode($data->getAttributeLabel('color')); ?>:</b>
-	<?php echo CHtml::encode($data->color); ?>
-	<br />
-
+		<p><?= (empty($data->wiki)) ? 'No wiki entry' : 'Have wiki entry' ?></p>
+		<?php /*
+		  <b><?php echo CHtml::encode($data->getAttributeLabel('wiki')); ?>:</b>
+		  <?php echo $md->transform($data->wiki); ?>
+		 */ ?>
+	</div>
 	<hr />
-
 </div>
+<div class="clearfix"></div>
