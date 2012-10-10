a740a3355d31705c1f427892cc66d05136fff5a8
gnysek <gnysek@gmail.com>
2012-10-10 16:07:14 +0200

    Small change to display wiki button and desc

--- protected/views/projects/_view.php
+++ protected/views/projects/_view.php
@@ -8,13 +8,18 @@
 	<div class="seven columns" style="padding-left: 10px; border-left: 20px solid <?= (!empty($data->color)) ? $data->color : 'black' ?>;">
 
 		<div class="right">
-			<?php echo CHtml::link('Update', array('update', 'id' => $data->id), array('class' => 'button secondary tiny')); ?>
+			<?php if (!empty($data->wiki)): ?>
+				<?php echo CHtml::link('Wiki', array('view', 'id' => $data->id), array('class' => 'button secondary tiny')); ?>
+			<?php endif; ?>
+			<?php echo CHtml::link('Edit', array('update', 'id' => $data->id), array('class' => 'button secondary tiny')); ?>
 			<?php echo CHtml::link('Delete', array('delete', 'id' => $data->id), array('class' => 'button secondary tiny')); ?>
 		</div>
 
 		<p>#<?= $data->id ?>. <strong><?php echo CHtml::link(CHtml::encode($data->name), array('view', 'id' => $data->id)); ?></strong></p>
 
-		<p><?= (empty($data->wiki)) ? 'No wiki entry' : 'Have wiki entry' ?></p>
+		<?php if (!empty($data->wiki)): ?>
+			<p>Check wiki entry &raquo;</p>
+		<?php endif; ?>
 		<?php /*
 		  <b><?php echo CHtml::encode($data->getAttributeLabel('wiki')); ?>:</b>
 		  <?php echo $md->transform($data->wiki); ?>
